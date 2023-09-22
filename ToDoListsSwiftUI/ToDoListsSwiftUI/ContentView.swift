8404b2063d099902b6d133915274bbe2abbbd6bf
Wilbert Devin Wijaya <54884570+wilbertdevin@users.noreply.github.com>
2023-09-22 13:22:10 +0700

    Add files via upload

---
+++ ToDoListsSwiftUI/ToDoListsSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoListsSwiftUI
+//
+//  Created by Wilbert Devin Wijaya on 22/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoListsSwiftUI/ToDoListsSwiftUI/ToDoListsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListsSwiftUIApp.swift
+//  ToDoListsSwiftUI
+//
+//  Created by Wilbert Devin Wijaya on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
